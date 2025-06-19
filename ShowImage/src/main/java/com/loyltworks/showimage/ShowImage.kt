b1df4d09867703e11f9b6a5d240e3969475d24d7
satyam <loyltwo3ks.developers@gmail.com>
2025-06-19 11:15:39 +0530

    initial commit

--- ShowImage/src/main/java/com/loyltworks/showimage/ShowImage.kt
+++ ShowImage/src/main/java/com/loyltworks/showimage/ShowImage.kt
@@ -4,22 +4,29 @@ import android.content.Context
 import android.graphics.BitmapFactory
 import android.widget.ImageView
 import kotlinx.coroutines.*
-import java.io.InputStream
 import java.net.URL
 
 class ShowImage {
 
-    fun showImage(context: Context, url: String, imageview: ImageView) {
+        fun showImage(context: Context, url: String, imageview: ImageView) {
+
+        imageview.tag=url
 
         GlobalScope.launch(Dispatchers.IO) {
             try {
                 val inputStream = URL(url).openStream()
                 val bitmap = BitmapFactory.decodeStream(inputStream)
                  withContext(Dispatchers.Main) {
-                    imageview.setImageBitmap(bitmap)
+                     if (imageview.tag == url) {
+                         imageview.setImageBitmap(bitmap)
+                     }
                 }
             } catch (e: Exception) {
-                imageview.setImageResource(R.drawable.user)
+                withContext(Dispatchers.Main) {
+                    if (imageview.tag == url) {
+                        imageview.setImageResource(R.drawable.user)
+                    }
+                }
             }
         }
 
